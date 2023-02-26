d69ff7ff11071ba51b9ce9cb6204592a3ada32cd
Ali <aghafari917@gmail.com>
2023-02-26 09:08:48 -0500

    Add project files.

---
+++ MultiplicationTable/MultiplicationTable.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MultiplicationTable.aspx.cs" Inherits="MultiplicationTable._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+        <section class="row" aria-labelledby="aspnetTitle">
+            <h1 id="aspnetTitle">Multiplication Table</h1>
+        </section>
+
+        <div class="row" style="float:left">
+             <section class="col-md-4" aria-labelledby="gettingStartedTitle">
+                 <asp:Label ID="Label1" runat="server" Text="Size: "></asp:Label>
+                 <asp:DropDownList ID="TableSizeList" runat="server"
+                      AutoPostBack="True"
+                      OnSelectedIndexChanged="Size_Selection_Change">
+                        <asp:ListItem Value="3"> 3</asp:ListItem>
+                        <asp:ListItem Value="4"> 4 </asp:ListItem>
+                        <asp:ListItem Value="5"> 5 </asp:ListItem>
+                        <asp:ListItem Value="6"> 6 </asp:ListItem>
+                        <asp:ListItem Value="7"> 7 </asp:ListItem>
+                        <asp:ListItem Value="8"> 8 </asp:ListItem>
+                        <asp:ListItem Value="9"> 9 </asp:ListItem>
+                        <asp:ListItem Value="10"> 10 </asp:ListItem>
+                        <asp:ListItem Value="11"> 11 </asp:ListItem>
+                        <asp:ListItem Value="12"> 12 </asp:ListItem>
+                        <asp:ListItem Value="13"> 13 </asp:ListItem>
+                        <asp:ListItem Value="14"> 14 </asp:ListItem>
+                        <asp:ListItem Value="15"> 15 </asp:ListItem>
+                 </asp:DropDownList>
+              </section>
+              <section class="col-md-4" aria-labelledby="gettingStartedTitle">
+                 <asp:Label ID="Label2" runat="server" Text="Format: "></asp:Label>
+                 <asp:DropDownList ID="FormatList" runat="server"
+                      AutoPostBack="True"
+                      OnSelectedIndexChanged="Format_Selection_Change">
+                      <asp:ListItem Selected="True" Value="1"> Int</asp:ListItem>
+                     <asp:ListItem  Value="2"> Decimal</asp:ListItem>
+                      <asp:ListItem Value="3"> Binary </asp:ListItem>
+                      <asp:ListItem Value="4"> Hex </asp:ListItem>
+                 </asp:DropDownList>
+              </section>
+        </div>
+        <div class="row">
+             <section class="col-md-4" aria-labelledby="gettingStartedTitle">
+               <asp:Table ID="MultiplicationTbl" runat="server" BorderStyle="Solid" CellPadding="5" CellSpacing="5" BorderWidth="2" BorderColor="#3399FF" BackColor="White" ></asp:Table>
+              </section>
+        </div>
+    </main>
+</asp:Content>
+
---
+++ MultiplicationTable/MultiplicationTable.aspx.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MultiplicationTable
+{
+    public partial class _Default : Page
+    {
+        private int tableSize = 10;
+        //int:1
+        //decimal :2
+        //binary :3
+        //hex :4
+        private int format = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+                return;
+            TableSizeList.SelectedIndex = TableSizeList.Items.IndexOf(TableSizeList.Items.FindByValue(tableSize.ToString()));
+
+            BuildTable();
+        }
+
+        private void BuildTable()
+        {
+            MultiplicationTbl.Rows.Clear();
+            tableSize = int.Parse(TableSizeList.SelectedItem.Value);
+            for (int i = 0; i <= tableSize; i++)
+            {
+                var row = new TableRow();
+                if (i == 0)
+                {
+                    //First Row
+                    row.Cells.Add(BuildCell("X"));
+                    for (int j = 1; j <= tableSize; j++)
+                    {
+                        BuildBorderCells(row, j.ToString());
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j <= tableSize; j++)
+                    {
+                        if (j == 0)
+                        {
+                            //First Column
+                            BuildBorderCells(row, i.ToString());
+                        }
+                        else
+                        {
+                            var cellValue = BuildFormat((i * j));
+                            var cell = BuildCell(cellValue);
+                            cell.ToolTip = $"{i}X{j}={cellValue}";
+                            if (i == j)
+                            {
+                                cell.BackColor = Color.LightBlue;
+                            }
+                            row.Cells.Add(cell);
+                        }
+                    }
+                }
+                MultiplicationTbl.Rows.Add(row);
+            }
+        }
+
+        private string BuildFormat(int v)
+        {
+            format = int.Parse(FormatList.SelectedItem.Value);
+            switch (format)
+            {
+                //int
+                case 1:
+                    return v.ToString();
+                //decimal
+                case 2:
+                    return v.ToString("#.00");
+                //binary
+                case 3:
+                    return Convert.ToString(v, 2);
+                //hex
+                case 4:
+                    return v.ToString("X");
+                default: return v.ToString();
+            }
+        }
+
+        protected void Size_Selection_Change(Object sender, EventArgs e)
+        {
+            BuildTable();
+        }
+
+        protected void Format_Selection_Change(Object sender, EventArgs e)
+        {
+            BuildTable();
+        }
+        private void BuildBorderCells(TableRow row, string text)
+        {
+            var cell = BuildCell(text);
+            cell.BackColor = Color.Beige;
+            row.Cells.Add(cell);
+        }
+        private TableCell BuildCell(string text)
+        {
+            var newCell = new TableCell
+            {
+                Text = text,
+            };
+            newCell.ApplyStyle(GetCommonStyle());
+            return newCell;
+        }
+        private TableItemStyle GetCommonStyle()
+        {
+            return new TableItemStyle
+            {
+                HorizontalAlign = HorizontalAlign.Center,
+                VerticalAlign = VerticalAlign.Middle,
+                Width = Unit.Pixel(100),
+                BorderColor = Color.Black,
+                BorderStyle = BorderStyle.Solid,
+                BorderWidth = 1
+            };
+        }
+    }
+}
